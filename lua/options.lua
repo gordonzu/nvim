abd2af1c70d80f06bdb6ca76ab7377fdc5295e2d
gordonzu <gordonzu@outlook.com>
2025-12-04 22:16:01 -0500

    initial commit:

---
+++ lua/options.lua
@@ -0,0 +1,18 @@
+require "nvchad.options"
+
+local o = vim.o
+
+o.cursorlineopt ='both'
+o.number = true
+o.relativenumber = true
+o.wrap = true
+o.ignorecase = true
+o.smartcase = true
+o.tabstop = 2
+o.shiftwidth = 2
+o.swapfile = false
+o.termguicolors = true
+o.signcolumn = "yes"
+o.undofile = true
+o.clipboard = "unnamedplus"
+o.winborder = "rounded"
---
+++ lua/plugins/init.lua
@@ -0,0 +1,126 @@
+
+-- init.lua
+
+
+return {
+  {
+    "stevearc/conform.nvim",
+    event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("oil").setup({
+        default_file_explorer = true,
+        restore_win_options = true,
+        skip_confirm_for_simple_edits = true,
+
+        float = {
+          padding = 2,
+          max_width = 80,
+          max_height = 35,
+          border = "rounded",
+          win_options = {
+            winblend = 0,
+          },
+        },
+
+        override = function(conf)
+          conf.row = 1
+          return conf
+        end,
+
+
+        view_options = {
+          show_hidden = true,
+          is_hidden_file = function(name)
+            return false
+          end,
+          natural_order = true,
+          sort = {
+            { "type", "asc" },
+            { "name", "asc" },
+          },
+        },
+
+        columns = {
+          "icon",
+          "permissions",
+          "size",
+          "mtime",
+        },
+      })
+    end,
+  },
+
+  {
+  "stevearc/oil.nvim",
+  opts = {},
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup({
+      default_file_explorer = true,
+      restore_win_options = true,
+      skip_confirm_for_simple_edits = true,
+
+      float = {
+        padding = 2,
+        max_width = 80,
+        max_height = 35,
+        border = "rounded",
+        win_options = {
+          winblend = 0,
+        },
+      },
+
+      override = function(conf)
+        conf.row = 1
+        return conf
+      end,
+
+      view_options = {
+        show_hidden = true,
+        is_hidden_file = function(name)
+          return false
+        end,
+        natural_order = true,
+        sort = {
+          { "type", "asc" },
+          { "name", "asc" },
+        },
+      },
+
+      columns = {
+        "icon",
+        "permissions",
+				"size",
+				"mtime",
+			},
+		})
+	end,
+}
+
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
