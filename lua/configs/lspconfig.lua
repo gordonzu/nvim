177ffda71f95aa7333f2fae230091f057cff9674
gordonzu <gordonzu@outlook.com>
2025-12-05 16:43:57 -0500

    fixed gopls

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,26 @@
+--------------------------------------------------------------------------------
+--- lspconfig.lua
+--------------------------------------------------------------------------------
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "pyright" }
+local servers = { "html", "cssls", "gopls", "clangd" }
 vim.lsp.enable(servers)
+
+vim.lsp.config('*', {
+  root_markers = { '.git', '.hg' },
+})
+
+vim.lsp.config('*', {
+  capabilities = {
+    textDocument = {
+      semanticTokens = {
+        multilineTokenSupport = true,
+      }
+    }
+  }
+})
+
+
+
 
 -- read :h vim.lsp.config for changing options of lsp servers
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,3 @@
-
 -- init.lua
 
 
@@ -65,53 +64,6 @@ return {
       })
     end,
   },
-
-  {
-  "stevearc/oil.nvim",
-  opts = {},
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("oil").setup({
-      default_file_explorer = true,
-      restore_win_options = true,
-      skip_confirm_for_simple_edits = true,
-
-      float = {
-        padding = 2,
-        max_width = 80,
-        max_height = 35,
-        border = "rounded",
-        win_options = {
-          winblend = 0,
-        },
-      },
-
-      override = function(conf)
-        conf.row = 1
-        return conf
-      end,
-
-      view_options = {
-        show_hidden = true,
-        is_hidden_file = function(name)
-          return false
-        end,
-        natural_order = true,
-        sort = {
-          { "type", "asc" },
-          { "name", "asc" },
-        },
-      },
-
-      columns = {
-        "icon",
-        "permissions",
-				"size",
-				"mtime",
-			},
-		})
-	end,
-}
 
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
