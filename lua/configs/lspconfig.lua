8d29432c0aa10754dc771fb0b777afe645f23432
gordonzu <gordonzu@outlook.com>
2025-12-05 18:04:24 -0500

    added dotnet debugging

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -6,21 +6,20 @@ require("nvchad.configs.lspconfig").defaults()
 local servers = { "html", "cssls", "gopls", "clangd", "pyright" }
 vim.lsp.enable(servers)
 
-vim.lsp.config('*', {
-  root_markers = { '.git', '.hg' },
+vim.lsp.config("*", {
+  root_markers = { ".git", ".hg" },
 })
 
-vim.lsp.config('*', {
+vim.lsp.config("*", {
   capabilities = {
     textDocument = {
       semanticTokens = {
         multilineTokenSupport = true,
-      }
-    }
-  }
+      },
+    },
+  },
 })
 
-
-
+vim.lsp.config("roslyn", {})
 
 -- read :h vim.lsp.config for changing options of lsp servers
